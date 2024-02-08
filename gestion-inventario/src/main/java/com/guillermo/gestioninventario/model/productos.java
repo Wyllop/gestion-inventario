e03b8bff0b7cbab7eaf4ec0781cc047b7d9f5a71
guill <guillermofdez12@gmail.com>
2024-02-08 12:30:33 +0100

    MejoraCodigo

--- gestion-inventario/src/main/java/com/guillermo/gestioninventario/model/productos.java
+++ gestion-inventario/src/main/java/com/guillermo/gestioninventario/model/productos.java
@@ -1,12 +1,10 @@
 package com.guillermo.gestioninventario.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 
 @Entity
+@Table(name = "productos")
 public class productos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
